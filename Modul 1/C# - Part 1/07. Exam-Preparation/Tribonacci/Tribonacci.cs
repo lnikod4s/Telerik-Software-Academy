59980ae5e61f2bd5bb9ef71ebd7cc6e6d4744fe1
lnikod4s <lyudmil.nikodimov@gmail.com>
2015-01-20 15:34:56 +0100

    Added new exam task solution

---
+++ Modul 1/C# - Part 1/07. Exam-Preparation/Tribonacci/Tribonacci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+class Tribonacci
+{
+	static void Main()
+	{
+		BigInteger a = BigInteger.Parse(Console.ReadLine());
+		BigInteger b = BigInteger.Parse(Console.ReadLine());
+		BigInteger c = BigInteger.Parse(Console.ReadLine());
+		List<BigInteger> nums = new List<BigInteger> { a, b, c };
+		int N = int.Parse(Console.ReadLine());
+
+		for (int i = 3; i < N; i++)
+		{
+			nums.Add(nums[i - 1] + nums[i - 2] + nums[i - 3]);
+		}
+		Console.WriteLine(nums.Last());
+	}
+}
