fc9b4a8c1e46cf6fbddac37490fd2a8d8fe343e6
lnikod4s <lyudmil.nikodimov@gmail.com>
2015-02-19 17:49:46 +0100

    Added new exam task solutions

---
+++ Modul 1/02. C# - Part 2/09. Exam-Prep/StrangeLandNumbers/StrangeLandNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+// C# Part 2 - 2013/2014 @ 24 Jan 2014 - Evening
+// 1. StrangeLand Numbers
+class StrangeLandNumbers
+{
+	static void Main()
+	{
+		string[] alpha = { "f", "bIN", "oBJEC", "mNTRAVL", "lPVKNQ", "pNWE", "hT" };
+		var input = Console.ReadLine();
+
+		var currLetter = new StringBuilder();
+		BigInteger result = 0;
+		foreach (var c in input)
+		{
+			currLetter.Append(c);
+			if (alpha.Contains(currLetter.ToString()))
+			{
+				int currDigit = Array.IndexOf(alpha, currLetter.ToString());
+				result *= 7;
+				result += currDigit;
+				currLetter.Clear();
+			}
+		}
+
+		Console.WriteLine(result);
+	}
+}
