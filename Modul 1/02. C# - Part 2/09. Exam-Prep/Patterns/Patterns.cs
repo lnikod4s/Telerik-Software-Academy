13fdcce98bb055b58a1b581d8f7aee6fa51070f8
lnikod4s <lyudmil.nikodimov@gmail.com>
2015-02-22 17:22:44 +0100

    Added new exam task solutions

---
+++ Modul 1/02. C# - Part 2/09. Exam-Prep/Patterns/Patterns.cs
@@ -0,0 +1,88 @@
+﻿using System;
+// C# Part 2 - 2013/2014 @ 22 Jan 2014 - Evening
+// 3. Patterns
+class Patterns
+{
+	static void Main()
+	{
+		int n = int.Parse(Console.ReadLine());
+		long[,] matrix = new long[n, n];
+		for (int i = 0; i < n; i++)
+		{
+			var line = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+			for (int j = 0; j < line.Length; j++)
+			{
+				matrix[i, j] = long.Parse(line[j]);
+			}
+		}
+
+		Console.WriteLine(Solve(matrix));
+	}
+
+	private static string Solve(long[,] matrix)
+	{
+		long maxSum = int.MinValue;
+		bool isSumFound = false;
+
+		for (int row = 0; row < matrix.GetLength(0); row++)
+		{
+			for (int col = 0; col < matrix.GetLength(1); col++)
+			{
+				if (ContainsPattern(matrix, row, col))
+				{
+					long patternSum = CalculatePatternSum(matrix, row, col);
+					if (maxSum < patternSum)
+					{
+						maxSum = patternSum;
+					}
+					isSumFound = true;
+				}
+			}
+		}
+
+		if (isSumFound)
+		{
+			return string.Format("YES {0}", maxSum);
+		}
+		else
+		{
+			long diagonalSum = 0;
+			for (int index = 0; index < matrix.GetLength(0); index++)
+			{
+				diagonalSum += matrix[index, index];
+			}
+
+			return string.Format("NO {0}", diagonalSum);
+		}
+	}
+
+	private static long CalculatePatternSum(long[,] matrix, int row, int col)
+	{
+		long sum = 0;
+		sum += matrix[row, col];
+		sum += matrix[row, col + 1];
+		sum += matrix[row, col + 2];
+		sum += matrix[row + 1, col + 2];
+		sum += matrix[row + 2, col + 2];
+		sum += matrix[row + 2, col + 3];
+		sum += matrix[row + 2, col + 4];
+
+		return sum;
+	}
+
+	private static bool ContainsPattern(long[,] matrix, int row, int col)
+	{
+		if (row + 2 >= matrix.GetLength(0) || col + 4 >= matrix.GetLength(1))
+		{
+			return false;
+		}
+		var result = matrix[row, col] + 1 == matrix[row, col + 1] &&
+					 matrix[row, col + 1] + 1 == matrix[row, col + 2] &&
+					 matrix[row, col + 2] + 1 == matrix[row + 1, col + 2] &&
+					 matrix[row + 1, col + 2] + 1 == matrix[row + 2, col + 2] &&
+					 matrix[row + 2, col + 2] + 1 == matrix[row + 2, col + 3] &&
+					 matrix[row + 2, col + 3] + 1 == matrix[row + 2, col + 4];
+
+		return result;
+	}
+}
---
+++ Modul 1/02. C# - Part 2/09. Exam-Prep/TRES4Numbers/TRES4Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+// C# Part 2 - 2013/2014 @ 22 Jan 2014 - Evening
+// 1. TRES4 Numbers
+class TRES4Numbers
+{
+	static readonly string[] alpha = { "LON+", "VK-", "*ACAD", "^MIM", "ERIK|", "SEY&", "EMY>>", "/TEL", "<<DON" };
+	static void Main()
+	{
+		
+
+		ulong inputNumber = ulong.Parse(Console.ReadLine());
+
+		// Convert the input decimal number to nonery number (9 base system)
+		List<string> tresNumFourNumber = new List<string>();
+		do
+		{
+			tresNumFourNumber.Add(alpha[inputNumber % 9]);
+			inputNumber /= 9;
+		}
+		while (inputNumber != 0);
+
+		tresNumFourNumber.Reverse(); // Reversing the number to get the real value
+		string result = string.Join(string.Empty, tresNumFourNumber);
+
+		Console.WriteLine(result);
+	}
+}
