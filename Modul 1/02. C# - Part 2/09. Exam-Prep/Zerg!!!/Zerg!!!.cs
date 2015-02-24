f68c38ac65025d5642f8fd31a79c6227b830f897
lnikod4s <lyudmil.nikodimov@gmail.com>
2015-02-24 17:01:15 +0100

    Added new exam task solutions

---
+++ Modul 1/02. C# - Part 2/09. Exam-Prep/Zerg!!!/Zerg!!!.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+// C# Part 2 - 2013/2014 @ 14 Sept 2013 - Evening
+// 1. Zerg!!!
+class Zerg
+{
+	static void Main()
+	{
+		string[] alpha = { "Rawr", "Rrrr", "Hsst", "Ssst", "Grrr", "Rarr", "Mrrr", "Psst", "Uaah", "Uaha", "Zzzz", "Bauu", "Djav", "Myau", "Gruh" };
+
+		var input = Console.ReadLine();
+
+		var currLetter = new StringBuilder();
+		BigInteger result = 0;
+		foreach (var c in input)
+		{
+			currLetter.Append(c);
+			if (alpha.Contains(currLetter.ToString()))
+			{
+				int currDigit = Array.IndexOf(alpha, currLetter.ToString());
+				result *= 15;
+				result += currDigit;
+				currLetter.Clear();
+			}
+		}
+
+		Console.WriteLine(result);
+	}
+}
