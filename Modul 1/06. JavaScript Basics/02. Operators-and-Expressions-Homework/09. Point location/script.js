8c2d93496d3f7b3ce9380bc8799e535eb418eff6
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2015-05-23 15:49:53 +0300

    Fixing slight mistake

--- Modul 1/06. JavaScript Basics/02. Operators-and-Expressions-Homework/09. Point location/script.js
+++ Modul 1/06. JavaScript Basics/02. Operators-and-Expressions-Homework/09. Point location/script.js
@@ -2,7 +2,7 @@
 // rectangle R(top=1, left=-1, width=6, height=2).
 
 function solveTask(x, y) {
-	var isInCircle = (x - 1) * (x - 1) + (y - 1) * (y - 1) <= 3;
+	var isInCircle = (x - 1) * (x - 1) + (y - 1) * (y - 1) <= 3 * 3;
 	var isOutOfRectangle = !((x >= -1 && x <= -1 + 6) && (y <= 1 && y >= 1 - 2));
 	return isInCircle && isOutOfRectangle;
 }
