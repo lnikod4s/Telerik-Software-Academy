bfc10ac12236b4bc18cdd7fb0ce2f964a36aebb9
lnikod4s <lyudmil.nikodimov@gmail.com>
2015-08-14 16:36:36 +0300

    Added thirteenth homework for the course 'HQC'

---
+++ Modul 2/HQC/13. Development-Tools-Homework/Log4NetExample/MainProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+namespace Log4NetExample
+{
+    internal class MainProgram
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof (MainProgram));
+
+        public static void Main()
+        {
+            XmlConfigurator.Configure();
+            Log.Info("Info logging");
+            try
+            {
+                throw new Exception("Exception!");
+            }
+            catch (Exception e)
+            {
+                Log.Error("This is my error", e);
+            }
+
+            Console.WriteLine("[any key to exit]");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Modul 2/HQC/13. Development-Tools-Homework/T4Example/SimpleT4.cs
@@ -0,0 +1,15 @@
+﻿namespace T4Example
+{
+    internal class HelloWorld
+    {
+        /// <summary>
+        ///     The Hello property
+        /// </summary>
+        public string Hello { get; set; }
+
+        /// <summary>
+        ///     The World property
+        /// </summary>
+        public string World { get; set; }
+    }
+}
