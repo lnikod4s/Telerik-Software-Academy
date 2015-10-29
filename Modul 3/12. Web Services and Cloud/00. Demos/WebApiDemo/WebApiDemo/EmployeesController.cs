73e19a520144f4fb2e3d099e8f4795593dc3ae92
Lyudmil Nikodimov <lyudmil.nikodimov@gmail.com>
2015-10-29 16:20:51 +0200

    Added WebApi Demo App

---
+++ Modul 3/12. Web Services and Cloud/00. Demos/WebApiDemo/WebApiDemo/EmployeesController.cs
@@ -0,0 +1,38 @@
+﻿namespace WebApiDemo
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    public class EmployeesController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Employee> Get()
+        {
+            return EmployeesRepository.GetAllEmployees();
+        }
+
+        // GET api/<controller>/5
+        public Employee Get(int id)
+        {
+            return EmployeesRepository.GetEmployee(id);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] Employee employee)
+        {
+            EmployeesRepository.InsertEmployee(employee);
+        }
+
+        // PUT api/<controller>/5
+        public void Put([FromBody] Employee employee)
+        {
+            EmployeesRepository.UpdateEmployee(employee);
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+            EmployeesRepository.DeleteEmployee(id);
+        }
+    }
+}
